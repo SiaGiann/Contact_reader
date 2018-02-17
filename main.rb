d9b6d7bfc238777c46c08b702181da68cbc6a7f5
SiaGiann <siayiannaki@gmail.com>
2018-02-17 21:45:49 +0100

    Assignment 3.2 reduce .org emails & remove comments

--- main.rb
+++ main.rb
@@ -9,19 +9,19 @@ def contacts_map
   read_contacts.map { |contact| {contact[:name] => contact[:phone]} }
 end
 
-# puts contacts_map
+puts contacts_map
 
 def phone_plus_one
   read_contacts.select { |contact| contact[:phone].include? "+1" }
 end
 
-#puts phone_plus_one
+puts phone_plus_one
 
 def email_org
   read_contacts.select { |contact| contact[:email].include? ".org" }
 end
 
-#puts email_org
+puts email_org
 
 def count_plus_one
   phone_plus_one.reduce(0) do |count, contact|
@@ -30,3 +30,9 @@ def count_plus_one
 end
 
 puts count_plus_one
+
+def count_emails_org
+  email_org.reduce(0) { |count, countact| count +=1 }
+end
+
+puts count_emails_org
