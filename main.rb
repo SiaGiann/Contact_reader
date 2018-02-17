e77a29bc0cf77c68f80922497558db2324dfe04f
SiaGiann <siayiannaki@gmail.com>
2018-02-17 21:13:51 +0100

    Assignment 2.1 filter

--- main.rb
+++ main.rb
@@ -5,8 +5,14 @@ def read_contacts
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
 
-def contacts_display
+def contacts_map
   read_contacts.map { |contact| {contact[:name] => contact[:phone]} }
 end
 
-puts contacts_display
+# puts contacts_map
+
+def phone_plus_one
+  read_contacts.select { |contact| contact[:phone].include? "+1" }
+end
+
+puts phone_plus_one
